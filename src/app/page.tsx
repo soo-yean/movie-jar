2ec09d0913c5f787e59d50c5c7cd18eab4bc0624
soo-yean <sooyean.jang@gmail.com>
2025-04-05 11:15:33 +0900

    INIT project

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Our Movie Jar</h1>
+
+      <div>
+        <input />
+        <button>ADD</button>
+      </div>
+
+      <div>
+        <button>PICK</button>
+      </div>
+    </main>
+  );
+}
