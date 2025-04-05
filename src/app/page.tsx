8ed56e067d7b26ea44cf6ac1d66888a541514970
soo-yean <sooyean.jang@gmail.com>
2025-04-05 17:30:46 +0900

    ADD feature: animation on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Image from "next/image";
+import jar from "../jar.png";
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
   const [movie, setMovie] = useState("");
@@ -47,8 +50,6 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center p-6 bg-pink-50 text-gray-800">
       <h1 className="text-4xl font-bold mb-6 text-rose-500">Our Movie Jar</h1>
 
-      <div>{randomMovie}</div>
-
       <div className="flex gap-2 mb-6">
         <input
           type="text"
@@ -65,14 +66,44 @@ export default function Home() {
         </button>
       </div>
 
-      <div className="mb-6">
+      {/* <div className="mb-6">
         <button
           onClick={drawRandomMovie}
           className="bg-lime-400 hover:bg-lime-500 text-white px-6 py-3 rounded-lg transition shadow-md"
         >
           PICK
         </button>
-      </div>
+      </div> */}
+
+      <AnimatePresence>
+        {randomMovie && (
+          <motion.div
+            key={randomMovie}
+            initial={{ scale: 0.6, y: 100, opacity: 0 }}
+            animate={{ scale: 1, y: 0, opacity: 1 }}
+            transition={{ type: "spring", stiffness: 200, damping: 10 }}
+            className="mt-4 bg-white  rounded-xl p-6 shadow-xl text-2xl text-rose-400 font-bold "
+          >
+            <motion.div
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1.05 }}
+              transition={{ delay: 0.3, type: "spring", stiffness: 300 }}
+              className="text-center italic"
+            >
+              {randomMovie}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <Image
+        onClick={drawRandomMovie}
+        src={jar}
+        className="cursor-pointer"
+        width={300}
+        height={500}
+        alt="movie jar"
+      />
     </main>
   );
 }
