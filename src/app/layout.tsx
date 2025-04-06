5810784445dfd28fcf57a91d9d7c0e685b7393f8
soo-yean <sooyean.jang@gmail.com>
2025-04-06 14:29:25 +0900

    REFACTOR: navbar to layout.tsx
    from page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopNav from "@/components/navbar/TopNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col bg-pink-50`}
       >
-        {children}
+        <TopNav />
+        <main className="flex-grow">{children}</main>
       </body>
     </html>
   );
