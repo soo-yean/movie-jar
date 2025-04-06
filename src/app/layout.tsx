8a42912b3584f1190f1104a84b610213fbabc936
soo-yean <sooyean.jang@gmail.com>
2025-04-06 16:51:35 +0900

    STYLE: change fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono, Quicksand } from "next/font/google";
+import { Quicksand } from "next/font/google";
+
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import TopNav from "@/components/navbar/TopNav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const quicksand = Quicksand({
   subsets: ["latin"],
+  weight: ["400", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +34,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col bg-pink-50`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col bg-pink-50`}
+        className={`${quicksand.className} antialiased flex flex-col bg-pink-50`}
       >
         <TopNav />
         <main className="flex-grow">
