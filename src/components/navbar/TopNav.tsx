20105d90613f96b5331d2b749052f6edb7ac4def
soo-yean <sooyean.jang@gmail.com>
2025-05-15 16:33:48 +0900

    FEATURE: change nav text based on the route

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-// import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 
 export default function TopNav() {
+  const pathname = usePathname();
+
   return (
     <motion.nav
       initial={{ y: -100 }}
@@ -11,14 +13,8 @@ export default function TopNav() {
       transition={{ type: "spring", stiffness: 100 }}
       className="bg-pink-200 text-pink-900 shadow-md py-4 px-8 rounded-b-3xl flex justify-center items-center"
     >
-      {/* <Link
-        href="/list"
-        className="text-2xl font-bold tracking-wide text-pink-700 hover:underline"
-      >
-        Hey Click ME!
-      </Link> */}
-      <h1 className="text-2xl font-bold tracking-wide">
-        Love you babe&#128151;
+      <h1 className="text-2xl font-bold tracking-wide transition-all duration-300 ease-in-out">
+        {pathname === "/" ? `Love you babe💗` : "Our List💗"}
       </h1>
     </motion.nav>
   );
