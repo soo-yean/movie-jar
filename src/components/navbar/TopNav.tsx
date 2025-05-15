de843ea02c0d8342015d23cc8437b29653045564
soo-yean <sooyean.jang@gmail.com>
2025-05-15 09:32:07 +0900

    TEMP: change top nav

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// import Link from "next/link";
 import { motion } from "framer-motion";
 
 export default function TopNav() {
@@ -10,8 +11,14 @@ export default function TopNav() {
       transition={{ type: "spring", stiffness: 100 }}
       className="bg-pink-200 text-pink-900 shadow-md py-4 px-8 rounded-b-3xl flex justify-center items-center"
     >
+      {/* <Link
+        href="/list"
+        className="text-2xl font-bold tracking-wide text-pink-700 hover:underline"
+      >
+        Hey Click ME!
+      </Link> */}
       <h1 className="text-2xl font-bold tracking-wide">
-        Hi Adam, how are you today?
+        Love you babe&#128151;
       </h1>
     </motion.nav>
   );
