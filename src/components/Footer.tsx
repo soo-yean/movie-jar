ccc508d9cadac1482f4e54882a4440bbf774b193
soo-yean <sooyean.jang@gmail.com>
2025-06-01 18:16:02 +0900

    FEATURE: add highlights on active menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,46 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GiHeartBottle } from "react-icons/gi";
 import { LuMessageSquareHeart } from "react-icons/lu";
 import { PiListHeartBold } from "react-icons/pi";
 
-export default function Footer() {
-  return (
-    <footer className="bg-pink-100 text-pink-700 shadow-inner fixed bottom-0 w-full py-3 px-4 flex justify-center items-center gap-10 z-50">
-      <Link
-        href="/"
-        className="flex flex-col items-center hover:scale-105 transition-all"
-      >
-        <GiHeartBottle className="text-xl" />
-        <span className="text-xs mt-1">Jar</span>
-      </Link>
+const navItems = [
+  { href: "/", icon: <GiHeartBottle className="text-xl" />, label: "Jar" },
+  {
+    href: "/list",
+    icon: <PiListHeartBold className="text-xl" />,
+    label: "List",
+  },
+  {
+    href: "/letters",
+    icon: <LuMessageSquareHeart className="text-xl" />,
+    label: "Letters",
+  },
+];
 
-      <Link
-        href="/list"
-        className="flex flex-col items-center hover:scale-105 transition-all"
-      >
-        <PiListHeartBold className="text-xl" />
-        <span className="text-xs mt-1">List</span>
-      </Link>
-      <Link
-        href="/letters"
-        className="flex flex-col items-center hover:scale-105 transition-all"
-      >
-        <LuMessageSquareHeart className="text-xl" />
-        <span className="text-xs mt-1">Letters</span>
-      </Link>
+export default function Footer() {
+  const pathname = usePathname();
+  console.log("ss", pathname);
+
+  return (
+    <footer className="bg-pink-100 shadow-inner fixed bottom-0 w-full py-3 px-4 flex justify-center items-center gap-10 z-50">
+      {navItems.map(({ href, icon, label }) => {
+        const isActive = pathname === href;
+        return (
+          <Link
+            key={href}
+            href={href}
+            className={`flex flex-col items-center hover:scale-105 transition-all ${
+              isActive ? "text-pink-700 font-bold" : "text-pink-500"
+            }`}
+          >
+            {icon}
+            <span className="text-xs mt-1">{label}</span>
+          </Link>
+        );
+      })}
     </footer>
   );
 }
